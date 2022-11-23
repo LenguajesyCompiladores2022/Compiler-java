1a09aeb07e786ed936828875531b64fbe68cf7ac
agus1355 <59593469+agus1355@users.noreply.github.com>
2022-11-22 22:40:16 -0300

    se agrega tipo de dato a nodo

--- src/main/java/lyc/compiler/files/Node.java
+++ src/main/java/lyc/compiler/files/Node.java
@@ -2,6 +2,8 @@ package lyc.compiler.files;
 
 public class Node {
 	String value;
+
+	String type;
 	Node left;
 	Node right;
 
@@ -9,6 +11,10 @@ public class Node {
 		this.value = value;
 		right = null;
 		left = null;
+	}
+	public Node(String value, String type){
+		this.value = value;
+		this.type = type;
 	}
 	public Node(String value, Node left, Node right){
 		this.value = value;
